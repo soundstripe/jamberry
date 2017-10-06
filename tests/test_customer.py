ea4501b91f75298a12600d219f414636dd783380
Steven James <steven@waitforitjames.com>
2017-10-05 21:05:18 -0400

    unit test for address property

---
+++ tests/test_customer.py
@@ -0,0 +1,11 @@
+from src.jamberry.customer import Customer
+
+
+def test_address_property():
+    c = Customer()
+    c.name = 'Ima Customer'
+    c.address_line_1 = '123 Nowhere St.'
+    c.address_city = 'Somewhere'
+    c.address_state = 'NV'
+    c.address_zip = '12345'
+    assert c.address == 'Ima Customer\n123 Nowhere St.\nSomewhere, NV 12345'
