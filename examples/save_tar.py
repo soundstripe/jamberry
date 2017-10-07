4796e1afbb21060c87355070c19e05730a59f65a
Steven James <steven@waitforitjames.com>
2017-10-06 20:15:56 -0400

    save_tar example updated

--- examples/save_tar.py
+++ examples/save_tar.py
@@ -3,22 +3,31 @@
 # This command line utility gives you a quick and easy TAR.csv file.
 # By default it saves the file into the current directory, but
 # you can specify the output directory as an optional argument.
+#
+# Username and password will be read from jamberry.ini.
+#
+# After the file is saved, it is launched in the system's default csv
+# viewer.
 
+import os
+import argparse
 from pathlib import Path
 from datetime import datetime
-import argparse
 import jamberry
 
 
 def save_tar(destination='.'):
-    """Fetch and save your current Team Activity Report CSV file into output_directory."""
+    """
+    Fetch and save your current Team Activity Report CSV file into output_directory.
+    defaults to current year/month, entire downline
+    """
     destination = Path(destination)
-    ws = jamberry.JamberryWorkstation()  # username and password will be read from jamberry.ini
-    csv_data = ws.fetch_team_activity_csv()  # defaults to current month, entire downline
+    ws = jamberry.JamberryWorkstation()
+    csv_data = ws.fetch_team_activity_csv()
     file_name = datetime.now().strftime('%Y-%m-%d %H.%M TAR.csv')
     file_path = destination / file_name
     file_path.write_bytes(csv_data)
-    return file_name
+    return file_path
 
 
 if __name__ == '__main__':
@@ -26,6 +35,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Fetch your current TAR from your workstation.')
     parser.add_argument('--output_directory', default='.', help='Specify the output directory')
+    parser.add_argument('--no-open', help='Specify this to simply save the file and exit')
     parser.parse_args()
 
-    print(save_tar(destination=output_directory))
+    path = save_tar(destination=output_directory)
+    os.startfile(path)
